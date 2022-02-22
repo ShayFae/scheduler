89a8f0554b56282b428dd3b676cfa4fb3f722ebd
ShayFae <ifrahnoor108@gmail.com>
2022-02-22 12:46:48 -0500

    refactor button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,28 @@
 import React from "react";
 import "components/Button.scss";
 import { action } from "@storybook/addon-actions/dist/preview";
+import classNames from "classnames";
 
 export default function Button(props) {
-// const {onClick, disabled} = props;
-let buttonClass = 'button';
-if (props.confirm) {
-   buttonClass += ' button--confirm';
-}
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
 
-if (props.danger) {
-   buttonClass += ' button--danger';
-}
-   // console.log({test})
+// function buttonClassFunc () {
+//    let buttonClass = 'button ';
+//    if (props.confirm) {
+//       buttonClass += classNames('button',{'button--confirm': true})
+//    } else if(props.danger) {
+//       buttonClass += classNames('button',{'button--danger': true})
+//    }
+//    return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
+// }
+
    return (
          <>
-            <button className={buttonClass}  disabled={props.disabled} onClick={props.onClick}>{props.children}</button>
+         {/* {buttonClassFunc()}; */}
+         <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
          </>
          );
 }
