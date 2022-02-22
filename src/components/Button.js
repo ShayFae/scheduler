73a845d3ebd5462bea7cbd9f57c84db913137a1f
ShayFae <ifrahnoor108@gmail.com>
2022-02-22 09:46:09 -0500

    add button style and onclick/disable buttons functional

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
-
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+// const {onClick, disabled} = props;
+let buttonClass = 'button';
+if (props.confirm) {
+   buttonClass += ' button--confirm';
+}
+
+if (props.danger) {
+   buttonClass += ' button--danger';
+}
+   // console.log({test})
+   return (
+         <>
+            <button className={buttonClass}  disabled={props.disabled} onClick={props.onClick}>{props.children}</button>
+         </>
+         );
 }
