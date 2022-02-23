8addf50464ef851577bc44c39c580156b0b2ca57
ShayFae <ifrahnoor108@gmail.com>
2022-02-23 12:49:38 -0500

    refactor interviewer and day components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,9 +3,7 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  // const {interviewerClass} = props
-  const {setInterviewer} = props
-  const interviewerList = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={setInterviewer} selected={interviewer.id === props.interviewer} {...interviewer}/>);
+  const interviewerList = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} setInterviewer={() => props.setInterviewer(interviewer.id)} selected={interviewer.id === props.interviewer} {...interviewer}/>);
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
