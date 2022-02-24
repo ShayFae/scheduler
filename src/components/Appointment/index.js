835651b25fc0088dcd5410fb11e789a7692d3184
ShayFae <ifrahnoor108@gmail.com>
2022-02-24 01:14:51 -0500

    can display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,22 @@
-// import Appointment from "components/Appointment";
-import React from "react";
+import React, { Fragment } from 'react';
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+  const {interview, time, student, interviewer} = props
+  // function pickDisplay(props) { 
+  //     return (interview ? <Show></Show> : <Empty></Empty>);
+  //   }
   return (
-    <article className="appointment">HELLO</article>
+    <Fragment>
+      <article className="appointment">
+        <Header time={time}/>
+        {/* {pickDisplay()} */}
+        {interview ? <Show student={student} interviewer={interviewer}></Show> : <Empty></Empty>}
+      </article>
+    </Fragment>
+
   );
 }
