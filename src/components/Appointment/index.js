cfd0ccae0598d472b84ddec4930e0ac62417c9cd
ShayFae <ifrahnoor108@gmail.com>
2022-02-28 18:57:55 -0500

    add confirmation before delete action

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Empty from "./Empty";
 import Form from './Form';
 import useVisualMode from 'hooks/useVisualMode';
 import Status from './Status';
+import Confirm from './Confirm';
 
 //MODE CONSTANTS
 const EMPTY = "EMPTY";
@@ -13,6 +14,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
 
@@ -22,6 +24,7 @@ export default function Appointment(props) {
   const {interviewers, student} = props
   // console.log(props.interview)
   
+  //Takes in user input infromation and puts it into the api 
   function save(name, interviewer) {
     const interview = {
       student: name,
@@ -29,17 +32,23 @@ export default function Appointment(props) {
     };
     transition(SAVING)
 
-    props.bookInterview(props.id, interview)
-    .then(() => transition(SHOW));
+      props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW));
     // console.log('STUDENT', interview.student)
     // console.log('interviewer', interview.interviewer)
   };
 
+  //If confirmed it will then delete
   function cancel() {
     transition(DELETING);
 
       props.cancelInterview(props.id)
       .then(() => transition(EMPTY))
+  }
+
+  //Once trashcan Icon is clicked it will goto CONFIRM MODE
+  function confirm() {
+    transition(CONFIRM)
   }
  
   return (
@@ -52,15 +61,14 @@ export default function Appointment(props) {
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
-            onDelete={cancel}
+            onDelete={confirm}
           />
         )}
-         {mode === DELETING && <Status message="DELETING" />}
+        {mode === DELETING && <Status message="DELETING" />}
         {mode === SAVING && <Status message="SAVING" />}
         {mode === CREATE && (<Form interviewers={interviewers}  onCancel={() => back(EMPTY)} onSave={save}/>)}       
-        {/* {interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}></Show> : <Empty></Empty>} */}
+        {mode === CONFIRM && <Confirm message="Are you sure you want to DELETE?" onConfirm={cancel} onCancel={() => back(EMPTY)} />}
       </article>
     </Fragment>
-
   );
 }
